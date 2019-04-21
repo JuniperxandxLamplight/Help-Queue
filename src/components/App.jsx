6909510aa88f6e0a90c718405a454f7781b92dec
Aurora Shido-Wagenet <fullmetalwoman@gmail.com>
2019-04-21 14:27:39 -0700

    add weekend week 2 homework - state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,20 +2,35 @@ import React from 'react';
 import Header from './Header';
 import TicketList from './TicketList';
 import NewTicketControl from './NewTicketControl';
-import MyStyledComponent from './MyStyledComponent';
 import { Switch, Route } from 'react-router-dom';
 
-function App(){
-  return (
-    <div>
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      masterTicketList: []
+    };
+    this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this);
+  }
+
+  handleAddingNewTicketToList(newTicket){
+    const newMasterTicketList = this.state.masterTicketList.slice();
+    newMasterTicketList.push(newTicket);
+    this.setState({masterTicketList: newMasterTicketList});
+  }
+
+  render(){
+    return (
+      <div>
       <Header/>
       <Switch>
-        <Route exact path='/' component={TicketList} />
-        <Route path='/newTicket' component={NewTicketControl} />
-       </Switch>
-      <MyStyledComponent/>
-    </div>
-  );
+        <Route exact path='/' render={()=><TicketList ticketList={this.state.masterTicketList} />} />
+        <Route path='/newticket' render={()=><NewTicketControl onNewTicketCreation={this.handleAddingNewTicketToList} />} />
+      </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
